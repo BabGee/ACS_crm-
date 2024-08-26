42ad6242b01a53d6af7b7af920b542c64b78099c
BabGee <jeremiah.george@syokinet.co.ke>
2024-08-26 09:57:40 +0300

    updates

--- ft_crm.py
+++ ft_crm.py
@@ -3,6 +3,25 @@ import requests
 import os
 
 
+# def check_device(sn):
+#     url = os.getenv('CHECKDV_API')
+#     headers = {'Content-Type': 'application/json'}
+
+#     payload = {
+#         "Device": {
+#             "Sn": sn,
+#         },
+#         "Creator": "rest_api",
+#         "AppId": "api",
+#         "CreatorPassword": ""
+#     }
+    
+#     response = requests.post(url, headers=headers, json=payload)
+#     if response.status_code == 200:
+#         print(f"Success: {record['username']}")
+#     else:
+#         print(f"Failed: {record['username']} with status code {response.status_code}")
+
 def api_call(data):
     url = os.getenv('USERINFO_API')
     headers = {'Content-Type': 'application/json'}
@@ -18,11 +37,12 @@ def api_call(data):
                 "Telephone": record['phone'],  
                 "Location": record['sub_id'],  
                 "Tag": record['olt'], 
-                "Id":record['sub_number'],  
+                "id":record['sub_number'],  
                 "Cust1": record['username'], 
                 "Cust2": record['password'],  
                 #"Cust7": record['address'], 
-                "Cust8": record['email'],                 
+                "Cust8": record['email'], 
+                'Cust9': f"https://billing.zoho.com/app/809359528#/subscriptions/{record['sub_id']}"           
             },
             "Parameters": [],
             "Creator": "syk_script",  
@@ -36,62 +56,47 @@ def api_call(data):
         else:
             print(f"Failed: {record['username']} with status code {response.status_code}")
 
-def check_device(sn):
-    url = os.getenv('CHECKDV_API')
-    headers = {'Content-Type': 'application/json'}
 
-    payload = {
-        "Device": {
-            "Sn": sn,
-        },
-        "Creator": "rest_api",
-        "AppId": "api",
-        "CreatorPassword": ""
-    }
-    
-    response = requests.post(url, headers=headers, json=payload)
-    if response.status_code == 200:
-        print(f"Success: {record['username']}")
-    else:
-        print(f"Failed: {record['username']} with status code {response.status_code}")
-
-olt_csv = pd.read_csv('data/syk_olt.csv', usecols=['SN','OLT', 'Username', 'Password'], dtype=str)
-subscriptions_df = pd.read_csv('data/subscriptions_with_phone_numbers_deduplicated.csv', dtype=str)
+# Load the data from CSV files
+olt_csv = pd.read_csv('data/syk_olt_p_11.csv', usecols=['SN', 'OLT', 'Username', 'Password'], dtype=str)
+subscriptions_df = pd.read_csv('data/subscriptions_with_phone_numbers_cleaned.csv', dtype=str)
 
 # Iterate over each row in olt_csv.
 for index, row in olt_csv.iterrows():
     username = row['Username']
-    serial_number = row['SN']
-
-    check_device(serial_number)
+    serial_number = row['SN'].replace("HWTC", "48575443")
 
     # Check if the username exists in subscriptions_with_phone_numbers_deduplicated.csv
-    matching_row = subscriptions_df[subscriptions_df['Username'] == username]
+    matching_rows = subscriptions_df[subscriptions_df['CF.Username'] == username]
     
-    if not matching_row.empty:
-        # Extract the email, phone, and name fields
-        email = matching_row.iloc[0]['EmailID']
-        phone = matching_row.iloc[0]['Phone']
-        name = matching_row.iloc[0]['CustomerName']
-        sub_id = matching_row.iloc[0]['SubscriptionID']
-        sub_no = matching_row.iloc[0]['SubscriptionNumber']
-        username_confirm = matching_row.iloc[0]['Username']
+    if not matching_rows.empty:
+        records = []
+        
+        # Iterate over all matching rows to prepare data
+        for _, matching_row in matching_rows.iterrows():
+            email = matching_row['EmailID']
+            phone = matching_row['Phone']
+            name = matching_row['CustomerName']
+            sub_id = matching_row['SubscriptionID']
+            sub_no = matching_row['SubscriptionNumber']
 
-        if username == username_confirm:
-            # Prepare the data to be sent to the external API
-            data = {
-                'serial_number': row['SN'],
+            records.append({
+                'serial_number': serial_number,
                 'olt': row['OLT'],
                 'username': username,
-                'password': row['password'],
+                'password': row['Password'],
                 'email': email,
                 'phone': phone,
                 'name': name,
                 'sub_id': sub_id,
                 'sub_number': sub_no,
-            }
+            })
+        print(f"RECORD>> {records}")    
 
-            api_call(data)
+        # Send the collected records to the API
+        api_call(records)
+    else:
+        print(f"No matching subscriptions found for username: {username}") 
 
 
 
--- migrate_onu.py
+++ migrate_onu.py
@@ -3,14 +3,19 @@ import os
 import pandas as pd
 
 from netmiko import ConnectHandler
+
 import logging
-logging.basicConfig(filename='netmiko_log.log', level=logging.DEBUG)
+logging.basicConfig(filename='netmiko_logs/netmiko_log.log', level=logging.DEBUG)
 logger = logging.getLogger("netmiko")
 
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 def extract_ontid_csv():
     """Extracts all ONT IDs from the CSV file."""
-    olt_csv = pd.read_csv('data/nrb_west_p4.csv', usecols=['ont_id'])
+    olt_csv = pd.read_csv('data/syk_olt_p_11.csv', usecols=['ont_id'])
     return olt_csv['ont_id'].tolist()
 
 def provision_ont():
@@ -30,38 +35,41 @@ def provision_ont():
             logger.info("BEGIN Execution")
             find_prompt = conn.find_prompt()
             if not '#' in find_prompt:
-                conn.enable()
-                logger.info("Connection established")
-                conn.send_command_timing('config', read_timeout=0)   
-                conn.send_command_timing("interface gpon 0/1", read_timeout=0)
+                # conn.enable()
+                # logger.info("Connection established")
+                # conn.send_command_timing('config', read_timeout=5)   
+                # conn.send_command_timing("interface gpon 0/13", read_timeout=5)
                 
                 # Extract all ONT IDs from the CSV
                 ont_ids = extract_ontid_csv()
                 
+                
                 for ont_id in ont_ids:
-                    logger.info(f"Configuring ONT ID: {ont_id}")
+                    logger.info(f"Migrating ONT ID: {ont_id}")
+                    conn.enable()
+                    logger.info("Connection established")
+                    conn.send_command_timing('config', read_timeout=5)   
+                    conn.send_command_timing("interface gpon 0/13", read_timeout=5)                    
                     config_commands = [
-                        f"ont ipconfig 4 {ont_id} dhcp vlan 91 priority 2", 
-                        f"ont tr069-server-config 4 {ont_id} profile-id 2"
+                        f"ont ipconfig 11 {ont_id} dhcp vlan 91 priority 2", 
+                        f"ont tr069-server-config 11 {ont_id} profile-id 2"
                     ]
+                    conn.enable()
                     conn.send_config_set(config_commands)
-                    conn.send_command_timing('config', read_timeout=0)
-                    service_port_cmd = (f"service-port vlan 91 gpon 0/1/4 ont {ont_id}"
-                                        "gemport 2 multi-service user-vlan 91"
-                                        "tag-transform translate inbound traffic-table index 8"
-                                        "outbound traffic-table index 8")
-                    conn.send_command_timing(service_port_cmd, read_timeout=0)
+                    conn.send_command_timing('config', read_timeout=10)
+                    service_port_cmd = f"service-port vlan 91 gpon 0/13/11 ont {ont_id} gemport 2 multi-service user-vlan 91 tag-transform translate inbound traffic-table index 8 outbound traffic-table index 8"
+                    conn.send_command_timing(service_port_cmd, read_timeout=10)
                     logger.info(f"ONT ID {ont_id} configured successfully.")
                 
-                logger.info("All ONTs provisioned successfully.")
+                logger.info("All ONTs Migrated successfully.")
             else:
                 logger.warning("Connection issue detected.")
 
     except Exception as e:
-        logger.error(f"Error during provisioning: {e}")
+        logger.error(f"Error during Migration: {e}")
 
 if __name__ == "__main__":
     try:
         provision_ont()
     except Exception as e:
-        logger.error(f"Provisioning failed: {e}")
+        logger.error(f"Migration failed: {e}")
